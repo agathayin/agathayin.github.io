e1b0fdbdc6b924b0438a06493e3014603d2d8528
agatha-yin <agathayin.sh@gmail.com>
2021-03-13 22:38:54 -0500

    scss fix for ul

--- js/header.js
+++ js/header.js
@@ -1,11 +1,13 @@
 const headerContent = `
     <nav>
         <a href="./index.html" class="logo"><img src="./images/logo.svg" alt="logo" height="40"/></a>
-        <img src="./images/menu.svg" alt="hambuger menu" width="40" class="mobile" rel="menuList" onclick="toggleMenu()"/>
-        <ul class="desktop" rel="headerMenu">
-            <li><a href="./index.html">Home</a></li>
-            <li><a href="./projects.html">Projects</a></li>
-        </ul>
+        <div class="menu">
+            <img src="./images/menu.svg" alt="hambuger menu" width="40" class="mobile" rel="menuList" onclick="toggleMenu()"/>
+            <ul class="desktop" rel="headerMenu">
+                <li><a href="./index.html">Home</a></li>
+                <li><a href="./projects.html">Projects</a></li>
+            </ul>
+        </div>
     </nav>
 `
 $(function(){
