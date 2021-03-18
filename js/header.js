6bdc99f5c3cfa16a46ca739eeacbad5431232836
agatha-yin <agathayin.sh@gmail.com>
2021-03-18 18:58:47 -0400

    heder and gallery style fix

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@ const headerContent = `
     <nav>
         <a href="`+document.location.origin+`/index.html" class="logo"><img src="`+document.location.origin+`/images/logo.svg" alt="logo" height="40"/></a>
         <div class="menu">
-            <img src="./images/menu.svg" alt="hambuger menu" width="40" class="mobile" rel="menuList" onclick="toggleMenu()"/>
+            <img src="`+document.location.origin+`/images/menu.svg" alt="hambuger menu" width="40" class="mobile" rel="menuList" onclick="toggleMenu()"/>
             <ul class="desktop" rel="headerMenu">
                 <li><a href="`+document.location.origin+`/index.html">Home</a></li>
                 <li><a href="`+document.location.origin+`/projects.html">Projects</a></li>
